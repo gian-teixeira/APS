1790e3a67faabe3bdeea2fa5fd80ee70086a2a7b
Giancarlo <giancarlo.oliveira.teixeira@gmail.com>
2024-03-29 10:10:44 -0300

    ...

---
+++ restaurant.py
@@ -0,0 +1,11 @@
+import calendar
+
+class Restaurant:
+    credit_price : float
+    operating_time : calendar.Date
+
+    def __init__():
+        pass
+
+print(Restaurant.credit_price)
+Restaurant.credit_price = 10
