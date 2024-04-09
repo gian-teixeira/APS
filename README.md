25862f2c8898fedf01d8c5108ccd1053e8a3e2e9
Giancarlo <giancarlo.oliveira.teixeira@gmail.com>
2024-04-09 00:47:36 -0300

    readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # RUConnect
 
+## Execução
+
+```
+python3 /src/ruconnect/programa.py
+```
+
 ## Diagrama de camadas
 
 ## Diagrama de comunicação
--- src/ruconnect/programa.py
+++ src/ruconnect/programa.py
@@ -5,7 +5,7 @@ from sys import argv
 class Programa:
     @staticmethod
     def main():
-        Persistence.set_database_folder(argv[1])
+        Persistence.set_database_folder("./data")
         root = View()
         root.mainloop()
 
