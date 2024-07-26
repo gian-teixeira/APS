46c70abb7cb1e6028dd0c9e71c82bd02daf20267
Reisdrj <davireisjesus@gmail.com>
2024-07-26 12:55:34 -0300

    Update credits

--- src/ruconnect/view/credit_manager.py
+++ src/ruconnect/view/credit_manager.py
@@ -1,5 +1,9 @@
 from model.user import Student
 from tkinter import ttk
+import tkinter as tk
+from view.entry import Entry
+from control.user import UserController
+from persistence.user import StudentPersistence
 
 class CreditDisplay(ttk.Frame):
     def __init__(self, user, *args, **kwargs):
@@ -7,9 +11,17 @@ class CreditDisplay(ttk.Frame):
         assert isinstance(user, Student)
 
         self.label = ttk.Label(self, text = f'Créditos : {user.credit}')
-        self.button = ttk.Button(self, text = 'Adicionar')
+        self.credits_entry = Entry("Insira a quantidade de cŕeditos")
+        self.button = ttk.Button(self, text = 'Adicionar', )
+        
+        # user.set_credit(float(int(self.credits_entry.get_content())))
 
     def pack(self, *args, **kwargs):
         self.label.pack()
+        self.credits_entry.pack()
         self.button.pack()
         super().pack(*args, **kwargs)
+
+    def compute_credits(self, user):
+        persistence = StudentPersistence()
+        controller = UserController(persistence)
