612c5d5133c503ae1fe0fccf534e5f70ed7a6ddc
Giancarlo <giancarlo.oliveira.teixeira@gmail.com>
2024-07-27 10:14:35 -0300

    working credit manager

--- src/ruconnect/view/credit_manager.py
+++ src/ruconnect/view/credit_manager.py
@@ -2,26 +2,36 @@ from model.user import Student
 from tkinter import ttk
 import tkinter as tk
 from view.entry import Entry
-from control.user import UserController
+from control.user import StudentController
 from persistence.user import StudentPersistence
+from model.session import Session
 
 class CreditDisplay(ttk.Frame):
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
         assert isinstance(user, Student)
 
-        self.label = ttk.Label(self, text = f'Créditos : {user.credit}')
+        self.credit_var = tk.StringVar()
+        self.credit_frame = ttk.Frame(self)
+        self.credit_text = ttk.Label(self.credit_frame, text = f'Créditos :')
+        self.credit_value = ttk.Label(self.credit_frame, textvariable = self.credit_var)
         self.credits_entry = Entry("Insira a quantidade de cŕeditos")
-        self.button = ttk.Button(self, text = 'Adicionar', )
-        
-        # user.set_credit(float(int(self.credits_entry.get_content())))
+        self.button = ttk.Button(self, text = 'Adicionar', command = self.compute_credits)
+
+        self.credit_var.set(Session.get_user().credit)
 
     def pack(self, *args, **kwargs):
-        self.label.pack()
+        self.credit_text.pack(side = tk.LEFT)
+        self.credit_value.pack(side = tk.RIGHT)
+        self.credit_frame.pack()
         self.credits_entry.pack()
         self.button.pack()
         super().pack(*args, **kwargs)
 
-    def compute_credits(self, user):
-        persistence = StudentPersistence()
-        controller = UserController(persistence)
+    def compute_credits(self):
+        controller = StudentController(StudentPersistence())
+        user = Session.get_user()
+        user.credit += int(self.credits_entry.get_content())
+        controller.delete(user.id)
+        controller.save(user)
+        self.credit_var.set(str(user.credit))
